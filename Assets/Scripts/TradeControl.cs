9a611fcce4051934b75d7320cdd07b47e86a6d9b
Shallowcat <joshR364@hotmail.com>
2023-10-08 22:55:57 -0400

    A 0.0.1.8

---
+++ Assets/Scripts/TradeControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TradeControl: MonoBehaviour
+{
+
+    public void Trade()
+    {
+
+    }
+
+
+    public void TradeComplete()
+    {
+
+    }
+
+
+
+}
--- Assets/Scripts/TradeScript.cs
+++ Assets/Scripts/TradeScript.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TradeScript : MonoBehaviour
 {
-    public void Trade()
+    [SerializeField] GameObject[] regularTradesOp1Left;
+    [SerializeField] GameObject[] regularTradesOp2Left;
+    [SerializeField] GameObject[] regularTradesOp1Right;
+    [SerializeField] GameObject[] regularTradesOp2Right;
+
+    [SerializeField] TMP_Text TLNum;
+    [SerializeField] TMP_Text TRNum;
+    [SerializeField] TMP_Text BLNum;
+    [SerializeField] TMP_Text BRNum;
+
+
+
+    public void RegularTrade()
     {
 
     }
 
-    public void TradeComplete()
-    {
 
-    }
 }
